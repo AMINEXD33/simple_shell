72b7e90c6d923f06ab6b0c1ab1b60ac0ec7d3b75
MohammedMaghraoui <136008817+MohammedMaghraoui@users.noreply.github.com>
2023-09-21 00:11:52 +0100

    Update mutsara.c

--- mutsara.c
+++ mutsara.c
@@ -1,10 +1,11 @@
 #include "shell.h"
 
 /**
- * exec_line - 
+ * exec_line - Execute a command line in the custom shell.
  *
- * @datash: 
- * Return: 
+ * @datash: Pointer to a data_shell structure containing shell data.
+ *
+ * Return: 0 on success, 1 on empty command, -1 on failure.
  */
 int exec_line(data_shell *datash)
 {
